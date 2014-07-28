2517e5c08e74f9e296a26248db723f0e64648084
sphasse <scott.hasse@gmail.com>
2014-07-27 19:44:00 -0500

    catch up

--- src/org/sector67/nsaaway/KeyManagerActivity.java
+++ src/org/sector67/nsaaway/KeyManagerActivity.java
@@ -1,4 +1,24 @@
+/*
+ * Copyright 2014 individual contributors as indicated by the @author 
+ * tags
+ * 
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>. 
+ */
 package org.sector67.nsaaway;
+
+import java.util.Iterator;
+import java.util.List;
 
 import org.sector67.otp.key.FileKeyStore;
 import org.sector67.otp.key.KeyException;
@@ -7,7 +27,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -15,6 +34,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+/**
+ * 
+ * @author scott.hasse@gmail.com
+ *
+ */
 public class KeyManagerActivity extends Activity {
 	
 	@Override
@@ -23,25 +47,42 @@ public class KeyManagerActivity extends Activity {
 		setContentView(R.layout.activity_key_manager);
 
         Button createTestKeys = (Button) findViewById(R.id.createTestKeysButton);
+        Button listKeys = (Button) findViewById(R.id.listKeysButton);
         
 		// Listen for a button event
         createTestKeys.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View arg0) {
 				try {
 					createTestKeys();
-					Toast.makeText(getApplicationContext(), "Test keys created", Toast.LENGTH_SHORT).show();
+					Toast.makeText(getApplicationContext(), getString(R.string.message_test_keys_created), Toast.LENGTH_SHORT).show();
 				} catch (KeyException e) {
 					//Toast.makeText(getApplicationContext(), "Error creating test keys: " + e.getMessage(), Toast.LENGTH_LONG).show();
-					createAlert("Test Key Creation Error", "An error occured while creating test keys: " + e.getMessage());
+					createAlert(getString(R.string.title_test_keys_creation_error), getString(R.string.message_test_keys_creation_error) + ": " + e.getMessage(), KeyManagerActivity.this);
 				}
 			}
 		});
-				
+
+		// Listen for a button event
+        listKeys.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View arg0) {
+				try {
+					List<String> keys = listKeys();
+					String result = "";
+					for (Iterator<String> iterator = keys.iterator(); iterator.hasNext();) {
+						String key = (String) iterator.next();
+						result = result + key + "\n";
+					}
+					createAlert(getString(R.string.title_key_list), result, KeyManagerActivity.this);
+				} catch (KeyException e) {
+					//Toast.makeText(getApplicationContext(), "Error creating test keys: " + e.getMessage(), Toast.LENGTH_LONG).show();
+					createAlert(getString(R.string.title_key_list_error), getString(R.string.message_key_list_error) + ": " + e.getMessage(), KeyManagerActivity.this);
+				}
+			}
+		});
 	}
 	
-	private void createAlert(String title, String message) {
-		AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
-		//TODO: extract string
+	private void createAlert(String title, String message, Context context) {
+		AlertDialog.Builder builder1 = new AlertDialog.Builder(context);
 		builder1.setTitle(title);
 		builder1.setMessage(message);
 		builder1.setCancelable(true);
@@ -55,6 +96,18 @@ public class KeyManagerActivity extends Activity {
 		alert11.show(); 
 
 	}
+
+	private List<String> listKeys() throws KeyException {
+		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+		String keyStorePath = sharedPref.getString(SettingsActivity.KEY_PREF_KEYSTORE_PATH, "");
+
+
+    	FileKeyStore store = new FileKeyStore(keyStorePath);
+   		store.init();
+   		List<String> keys = store.listKeys();
+   		return keys;
+	}
+
 	
 	private void createTestKeys() throws KeyException {
 		// /storage/extSdCard/Android/data/org.sector67.nsaaway/files/
@@ -66,7 +119,11 @@ public class KeyManagerActivity extends Activity {
    		store.init();
    		store.deleteKey("encrypt-key");
    		store.deleteKey("decrypt-key");
+   		store.deleteKey("alice-key");
+   		store.deleteKey("bob-key");
 		store.generateKey("encrypt-key", 1000);
    		store.copyKey("encrypt-key", "decrypt-key");
+		store.generateKey("alice-key", 1000);
+		store.generateKey("bob-key", 1000);
 	}
 }
