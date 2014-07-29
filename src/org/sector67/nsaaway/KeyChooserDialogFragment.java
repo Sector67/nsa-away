1cd084733d54980f7b68c368a6b7353e5d5d0f58
sphasse <scott.hasse@gmail.com>
2014-07-28 22:59:06 -0500

    copywrite file updates

--- src/org/sector67/nsaaway/KeyChooserDialogFragment.java
+++ src/org/sector67/nsaaway/KeyChooserDialogFragment.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2014 individual contributors as indicated by the @author 
+ * tags
+ * 
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>. 
+ */
 package org.sector67.nsaaway;
 
 import java.util.Iterator;
@@ -15,6 +32,11 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
+/**
+ * 
+ * @author scott.hasse@gmail.com
+ *
+ */
 public class KeyChooserDialogFragment extends DialogFragment {
 
     @Override
--- src/org/sector67/nsaaway/key/KeyUtils.java
+++ src/org/sector67/nsaaway/key/KeyUtils.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2014 individual contributors as indicated by the @author 
+ * tags
+ * 
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>. 
+ */
 package org.sector67.nsaaway.key;
 
 import org.sector67.nsaaway.SettingsActivity;
@@ -11,6 +28,11 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+/**
+ * 
+ * @author scott.hasse@gmail.com
+ *
+ */
 public class KeyUtils {
 	public static KeyStore getKeyStore(Context context) throws KeyException {
 		// /storage/extSdCard/Android/data/org.sector67.nsaaway/files/
